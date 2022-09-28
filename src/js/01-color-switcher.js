a6e082084d7367daaa311c71b35714c16eb62da5
Serj96 <99637869+Serj96@users.noreply.github.com>
2022-09-28 20:50:59 +0300

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
 
+    const containerEl = document.querySelector('body');
+    const startBtn = document.querySelector('[data-start]');
+    const stopBtn = document.querySelector('[data-stop]');
+
+    let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+    containerEl.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+    startBtn.disabled = false;
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    
+});
